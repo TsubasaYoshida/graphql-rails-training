bc95c3b9552409869ef1e05b2f551c43f5d16cce
TsubasaYoshida <tsubasa.yoshida.business@gmail.com>
2020-11-05 20:28:29 +0900

    リゾルバを記述する

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,17 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    field :articles, [Types::ArticleType], null: false
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def articles
+      Article.all
+    end
+
+    field :article, Types::ArticleType, null: false do
+      argument :id, Int, required: false
+    end
+
+    def article(id:)
+      Article.find(id)
     end
   end
 end
