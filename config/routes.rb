39e665ba4db89752ad86e3c98ad597648930870e
TsubasaYoshida <tsubasa.yoshida.business@gmail.com>
2020-11-05 20:50:28 +0900

    rails g controller home index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  root 'home#index'
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
