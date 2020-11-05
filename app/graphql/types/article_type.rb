dff9772fa95e721f72f982d388b9e19685846c23
TsubasaYoshida <tsubasa.yoshida.business@gmail.com>
2020-11-05 21:24:01 +0900

    rails g graphql:object articleでよかったっぽい

--- app/graphql/types/article_type.rb
+++ app/graphql/types/article_type.rb
@@ -5,8 +5,5 @@ module Types
     field :body, String, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :id, ID, null: false
-    field :title, String, null: false
-    field :body, String, null: false
   end
 end
